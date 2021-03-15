8771a2cdc06117a17ad493d6499551cc48701e05
mukhammadarifin <54782878+mukhammadarifin@users.noreply.github.com>
2021-03-15 23:05:18 +0700

    Upload Project Minggu 6

---
+++ Minggu 6/reduxeprakv2/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import MainReducer from './reducers/MainReducer'
+import App from './App'
+import Table from './containers/Table'
+import CreateTodo from './containers/CreateTodo'
+import registerServiceWorker from './registerServiceWorker'
+const store = compose(
+  window.devToolsExtension ? window.devToolsExtension() : (f) => f,
+)(createStore)(MainReducer)
+ReactDOM.render(
+  <Provider store={store}>
+    <CreateTodo />
+    <Table />
+  </Provider>,
+  document.getElementById('root'),
+)
+registerServiceWorker()
