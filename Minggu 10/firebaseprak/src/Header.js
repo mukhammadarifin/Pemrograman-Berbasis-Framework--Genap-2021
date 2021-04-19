b99b1aa7c5391e1f6f459fbccb4eca8e5ceac5b5
mukhammadarifin <54782878+mukhammadarifin@users.noreply.github.com>
2021-04-19 09:31:30 +0700

    Update Project Modul 10 Praktikum

---
+++ Minggu 10/firebaseprak/src/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import routes from "./routes";
+import {Link} from "react-router-dom";
+
+const Header = () => (
+    <ul className="nav">
+        {routes.map((route, i) =>(
+            <li key={i}>
+                <Link to={route.path}>
+                    {route.name}
+                </Link>
+            </li>
+        ))}
+    </ul>
+);
+
+export default Header;
--- Minggu 10/firebaseprak/src/index.js
+++ Minggu 10/firebaseprak/src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, {useState} from "react";
+import ReactDOM from "react-dom";
+import {Switch, BrowserRouter as Router, Route} from "react-router-dom";
+import routes from "./routes.js";
+import Header from "./Header.js";
+import "./style.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+export const AuthContext = React.createContext(null);
 
+function App() {
+  const [isLoggedIn, setLoggedIn] = useState(false);
+
+  return (
+    <AuthContext.Provider value={{isLoggedIn, setLoggedIn}}>
+      Is logged in? {JSON.stringify(isLoggedIn)}
+      <div className="App">
+        <Router>
+          <Header />
+
+          <Switch>
+            {routes.map(route => (
+              <Route
+                key={route.path}
+                path={route.path}
+                exact={route.exact}
+                component={route.main}
+              />
+            ))}
+          </Switch>
+        </Router>
+      </div>
+    </AuthContext.Provider>
+  );
+}
+
+const rootElement = document.getElementById("root");
+reactDOM.render(<App />, rootElement);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
