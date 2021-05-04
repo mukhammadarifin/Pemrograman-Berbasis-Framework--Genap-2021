a1d0ba8636af564b1f71d3c8161cebd5f4affd84
mukhammadarifin <54782878+mukhammadarifin@users.noreply.github.com>
2021-05-05 01:43:49 +0700

    Upload Project Laravel

---
+++ Minggu 11.5/example-app1/resources/views/biodata.blade.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <style type="text/css">
+      #wrapper {
+        display: flex;
+        border: 2px solid white;
+      }
+      #first {
+        margin-left: 40px;
+        margin-bottom: 20px;
+        margin-top: 20px;
+        border: 1px
+      }
+      #second {
+        margin-left: 30px;
+        margin-bottom: 20px;
+        margin-top: 20px;
+        border: 1px
+      }
+      h2.a {
+	      text-align: center;
+        color: #ffffff;
+      }
+      #image {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+      }
+      body {
+        background-color: #4B0082;
+      }
+  </style>
+
+    <title>My Biodata</title>
+  </head>
+  <body>
+    <div class="container" >
+      <h2 class="a">Biodata Pembuat Project Laravel</h2>
+      <hr/>
+      <div id="wrapper"> 
+        <div class="card" style="width:500px" id="first">
+          <br/>
+          <img id="image" class="card-img-top" src="https://avatars.githubusercontent.com/u/54782878?v=4" alt="Card image" style="width:70%">
+        <div class="card-body">
+      </div>
+    </div>
+    <div class="card" style="width:500px" id="second">
+      <div class="card-body">
+        <h4 class="card-title">Nama : Mukhammad Arifin</h4>
+        <p class="card-text">Mukhammad Arifin adalah seorang Web Developer & Android Developer</p>
+      </div>
+    </div>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
